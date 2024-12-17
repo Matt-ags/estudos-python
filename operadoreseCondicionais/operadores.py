3dbacc56a12b159404fc38dc8e42a4ce16e5b785
Matt-ags <mateusabnelocca@gmail.com>
2024-12-17 08:51:18 -0300

    operadores.py adicionados

---
+++ operadoreseCondicionais/operadores.py
@@ -0,0 +1,19 @@
+numero_1 = 20
+numero_2 = 10
+
+resultado = numero_1 + numero_2
+
+print("O resultado é:", resultado) # para juntar a variavel com o texto, é só colocar uma vírgula.
+# Temos os operadores de -, *, /, os comuns.
+# AO DIVIDIR, ELE TRANSFORMA UM NUMERO INTEIRO EM FLOAT (COM VÍRGULA)
+
+# ** -> É a potencia
+# % -> É o resto da divisão
+# // -> Divisão inteira, diferente do /, que deixa em float, este pega a parte inteira
+
+# - -> negação:
+
+resultado = numero_1 + -numero_2 #Ele transforma o numero 2 FIQUE O OPOSTO DELE.
+
+print("O resultado é:", resultado)
+
