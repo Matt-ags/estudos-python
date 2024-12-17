46ba24c40ff5fdcc061688444d9946954a269b9d
Matt-ags <mateusabnelocca@gmail.com>
2024-12-17 09:02:40 -0300

    operadores_condicionais.py adicionados

---
+++ operadoreseCondicionais/operadores_condicionais.py
@@ -0,0 +1,8 @@
+idade = 18
+
+if idade < 18: # Lembre-se destes dois pontos
+    print(idade ,"Anos, Menor de idade")
+elif idade == 18: #Lembrando que é sinal de extritamente igual
+    print("Maior de idade com exatos 18 anos.")
+else:
+    print(idade ,"Anos, maior de idade")
